eb079679ae0e48237c3e5092dd506bf07914571e
Naveen Akash <naveen.akash0904@gmail.com>
2023-10-26 20:17:10 +0530

    Implemented Custom Cursor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import NameOverlay from "./components/name-overlay/NameOverlay";
 import "./App.css";
 import AboutMe from "./components/aboutMe/AboutMe";
@@ -10,43 +10,49 @@ import Projects from "./components/projects/Projects";
 import Background from "./components/background/Background";
 import MobilePatternLeft from "./components/mobilePattern-left/MobilePatternLeft";
 import MobilePatternRight from "./components/mobilePattern-Right/MobilePatternRight";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
 
-      document
-        .querySelector(".topRight")
-        .classList.toggle("scrolled", scrollPosition > 0);
+    document
+      .querySelector(".topRight")
+      .classList.toggle("scrolled", scrollPosition > 0);
 
-      document
-        .querySelector(".mobilePatternRight")
-        .classList.toggle("scrolled", scrollPosition > 0);
+    document
+      .querySelector(".mobilePatternRight")
+      .classList.toggle("scrolled", scrollPosition > 0);
 
-      document
-        .querySelector(".mobilePatternLeft")
-        .classList.toggle("scrolled", scrollPosition > 0);
+    document
+      .querySelector(".mobilePatternLeft")
+      .classList.toggle("scrolled", scrollPosition > 0);
 
-      document
-        .querySelector(".bottomLeft")
-        .classList.toggle("scrolled", scrollPosition > 0);
-      // Add similar logic for other components
-
-    };
-
-    // Attach the scroll event listener
-    window.addEventListener("scroll", handleScroll);
-
-    // Clean up the event listener on component unmount
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-
+    document
+      .querySelector(".bottomLeft")
+      .classList.toggle("scrolled", scrollPosition > 0);
+  };
+  window.addEventListener("scroll", handleScroll);
+  const width = window.innerWidth;
   return (
     <>
+      {width > 770 ? (
+        <AnimatedCursor
+          innerSize={8}
+          outerSize={40}
+          innerScale={0}
+          outerScale={3}
+          outerStyle={{
+            mixBlendMode: "difference",
+            backgroundColor: "white",
+          }}
+          innerStyle={{
+            backgroundColor: "white",
+            mixBlendMode: "difference",
+          }}
+        />
+      ) : null}
+
       <div className="main" id="home">
         <Background />
         <MobilePatternLeft />
@@ -58,7 +64,7 @@ function App() {
       </div>
       <AboutMe id="about" />
       <Projects id="projects" />
-      <BottomBar/>
+      <BottomBar />
       {/* <p className="desktop-view">View in desktop for better experience</p> */}
     </>
   );
