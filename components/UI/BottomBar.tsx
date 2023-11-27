cc5de83a8fd357ef1884b7dd99db052e0d7de5d6
Naveen Akash <naveen.akash0904@gmail.com>
2023-11-27 20:50:38 +0530

    Static to dynamic items in bottom bar

--- components/UI/BottomBar.tsx
+++ components/UI/BottomBar.tsx
@@ -1,111 +1,99 @@
 import React from "react";
 import { Link } from "react-scroll";
 import styles from "../../styles/BottomBar.module.css";
-import Image from 'next/image';
-import instagramImage from '../../assets/images/Instagram.png';
-import linkedinImage from '../../assets/images/Linkedin.png';
-import githubImage from '../../assets/images/GitHub.png';
+import Image, { StaticImageData } from "next/image";
+import instagramImage from "../../assets/images/Instagram.png";
+import linkedinImage from "../../assets/images/Linkedin.png";
+import githubImage from "../../assets/images/GitHub.png";
+
+const socialButtons: {
+  title: string;
+  image: StaticImageData;
+  backgroundColor: string;
+  link: string;
+}[] = [
+  {
+    title: "Instagram",
+    image: instagramImage,
+    backgroundColor: "rgba(228, 64, 95, 0.5)",
+    link: "https://www.instagram.com/_n.a.ve.e.n_/",
+  },
+  {
+    title: "LinkedIn",
+    image: linkedinImage,
+    backgroundColor: "rgba(0, 123, 182, 0.5)",
+    link: "https://www.instagram.com/_n.a.ve.e.n_/",
+  },
+  {
+    title: "GitHub",
+    image: githubImage,
+    backgroundColor: "rgba(0, 0, 0, 0.3)",
+    link: "https://www.instagram.com/_n.a.ve.e.n_/",
+  },
+];
+
+const navLinks: {
+  title: string;
+  to: string;
+}[] = [
+  {
+    title: "HOME",
+    to: "home",
+  },
+  {
+    title: "ABOUT",
+    to: "aboutMe",
+  },
+  {
+    title: "PROJECTS",
+    to: "projects",
+  },
+];
 
 const BottomBar: React.FC = () => {
   return (
     <div className={styles.bottomBar}>
       <nav>
-        <Link
-          to="home"
-          smooth={true}
-          offset={0} // Adjust the offset if needed
-          duration={500}
-          className={styles.navLink}
-        >
-          HOME
-        </Link>
-        <Link
-          to="aboutMe"
-          smooth={true}
-          offset={50} // Adjust the offset if needed
-          duration={500}
-          className={styles.navLink}
-        >
-          ABOUT
-        </Link>
-        <Link
-          to="projects"
-          smooth={true}
-          offset={50} // Adjust the offset if needed
-          duration={500}
-          className={styles.navLink}
-        >
-          PROJECTS
-        </Link>
+        {navLinks.map((item) => {
+          return (
+            <Link
+            key={item.to}
+              to={item.to}
+              smooth={true}
+              offset={0} // Adjust the offset if needed
+              duration={500}
+              className={styles.navLink}
+            >
+              {item.title}
+            </Link>
+          );
+        })}
       </nav>
       <div className={styles.socialButtonDiv}>
-        <SocialButton></SocialButton>
+      {socialButtons.map((button) => (
+        <a
+          href={button.link}
+          style={{ textDecoration: "none" }}
+          key={button.title}
+        >
+          <button
+            className={styles.socialButton}
+            style={{ backgroundColor: button.backgroundColor }}
+          >
+            <Image
+              className={styles.socialImage}
+              src={button.image}
+              alt=""
+              width={30}
+              height={30}
+              style={{ padding: "10px" }}
+            />
+            <p className={styles.socialText}>{button.title}</p>
+          </button>
+        </a>
+      ))}
       </div>
     </div>
-  );
-};
-
-const SocialButton: React.FC = () => {
-  return (
-    <>
-      <a
-        href="https://github.com/NaveenAkash-K"
-        style={{ textDecoration: "none" }}
-      >
-        <button
-          className={styles.socialButton + " " + styles.GitHub}
-          style={{ backgroundColor: "rgba(0, 0, 0, 0.3)" }}
-        >
-          <Image
-            className={styles.socialImage}
-            src={githubImage}
-            alt=""
-            width={30}
-            height={30}
-            style={{ padding: "10px" }}
-          />
-          <p className={styles.socialText}>GitHub</p>
-        </button>
-      </a>
-      <a
-        href="https://www.linkedin.com/in/naveen-akash/"
-        style={{ textDecoration: "none" }}
-      >
-        <button
-          className={styles.socialButton}
-          style={{ backgroundColor: "rgba(0, 123, 182, 0.5)" }}
-        >
-          <Image
-            className={styles.socialImage}
-            src={linkedinImage}
-            alt=""
-            width={30}
-            height={30}
-            style={{ padding: "10px" }}
-          />
-          <p className={styles.socialText}>Linkedin</p>
-        </button>
-      </a>
-      <a
-        href="https://www.instagram.com/_n.a.ve.e.n_/"
-        style={{ textDecoration: "none" }}
-      >
-        <button
-          className={styles.socialButton}
-          style={{ backgroundColor: "rgba(228, 64, 95, 0.5)" }}
-        >
-          <Image
-            className={styles.socialImage}
-            src={instagramImage}
-            alt=""
-            width={30}
-            height={30}
-            style={{ padding: "10px" }}
-          />
-          <p className={styles.socialText}>Instagram</p>
-        </button>
-      </a>
-    </>
   );
 };
 
