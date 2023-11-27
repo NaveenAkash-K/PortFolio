1131b3b5be60c4dee9404dd9dd2c7c1a8fca3f17
Naveen Akash <naveen.akash0904@gmail.com>
2023-11-27 19:25:05 +0530

    Changed ESLint rules

--- components/sections/AboutPage.tsx
+++ components/sections/AboutPage.tsx
@@ -1,13 +1,13 @@
-import styles from '../../styles/AboutPage.module.css';
+import styles from "../../styles/AboutPage.module.css";
 
 const AboutPage: React.FC = () => {
   return (
-    <div className={styles.aboutMe} id='aboutMe'>
+    <div className={styles.aboutMe} id="aboutMe">
       <p className={styles.heading}>ABOUT</p>
       <p className={styles.description}>
         Hey there! I'm Naveen Akash, A tech enthusiast and a 3rd-year B.Tech IT
         student. Fluent in Flutter, React, C, C++, Python, JS, and Dart. I speak
-        both human and machine languages. 🚀 Beyond the realms of code, I'm a UI
+        both human and machine languages. 🚀 Beyond the realms of code, I"m a UI
         design mixologist, blending aesthetics with functionality. When I'm not
         crafting digital experiences, you'll find me on a mission to make the
         web a cooler place. Join the journey, and let's redefine the online
